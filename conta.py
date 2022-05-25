ff84c6fb311bfbd8fd39378e842ea4a08577c177
Rafael Carvalho <rafa_rafael_lucas@hotmail.com>
2022-05-24 23:20:42 -0300

    Atualização dos métodos
    Adição de métodos visando o encapsulamento do código;
    Implementação de atributos privados;
    Implementação de getters e setters para acessar e modificar atributos.

--- conta.py
+++ conta.py
@@ -4,16 +4,51 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print(f'Construindo objeto... {self}')
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f'Saldo de {self.saldo} do titular {self.titular}')
+        print(f'Saldo de {self.__saldo} do titular {self.__titular}')
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_para_saque = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_para_saque
 
     def saca(self, valor):
-        self.saldo -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print(f'O valor {valor} passou o limite')
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return '001'
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
